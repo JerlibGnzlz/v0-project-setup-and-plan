c4e4810f0e65cdfbd4196058917e7c8beaee5c2c
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-12-13 05:01:47 -0300

    fix: Importar NotificationsModule en CredencialesMinisterialesModule
    - Agregar import de NotificationsModule para habilitar notificaciones
    - Permitir que el servicio envíe notificaciones a admins

--- backend/src/modules/credenciales-ministeriales/credenciales-ministeriales.module.ts
+++ backend/src/modules/credenciales-ministeriales/credenciales-ministeriales.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { CredencialesMinisterialesService } from './credenciales-ministeriales.service'
 import { CredencialesMinisterialesController } from './credenciales-ministeriales.controller'
 import { PrismaModule } from '../../prisma/prisma.module'
+import { NotificationsModule } from '../notifications/notifications.module'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NotificationsModule],
   controllers: [CredencialesMinisterialesController],
   providers: [CredencialesMinisterialesService],
   exports: [CredencialesMinisterialesService],
